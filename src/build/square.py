5bb35787a9bb07c38db46ad51b2d5544798efe4b
FanZy0127 <fanzy28@hotmail.fr>
2022-06-20 22:08:21 +0200

    Setting of methods checking the presence of a piece on a square, and the team of this piece.

--- src/build/square.py
+++ src/build/square.py
@@ -8,3 +8,23 @@ class Square:
 
     def has_piece(self):
         return self.piece is not None
+
+    def is_empty(self):
+        return not self.has_piece()
+
+    def has_team_piece(self, color):
+        return self.has_piece() & self.piece.color == color
+
+    def has_opponent_piece(self, color):
+        return self.has_piece() & self.piece.color != color
+
+    def is_empty_or_has_an_opponent_piece(self, color):
+        return self.is_empty() | self.has_opponent_piece(color)
+
+    @staticmethod
+    def is_in_range(*args):
+        for arg in args:
+            if arg not in range(8):
+                return False
+
+        return True
