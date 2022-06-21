8ca6ba3af5787afeb2ca66dabd9fd0371c497948
FanZy0127 <fanzy28@hotmail.fr>
2022-06-21 16:20:37 +0200

    Fix of the undo-drag part for the movement of a piece, to allow it to stay on its new square and disappearing from its previous one.

--- src/build/move.py
+++ src/build/move.py
@@ -4,3 +4,6 @@ class Move:
     def __init__(self, base_square, final_square):
         self.base_square = base_square
         self.final_square = final_square
+
+    def __eq__(self, other):
+        return self.base_square == other.base_square and self.final_square == other.final_square
