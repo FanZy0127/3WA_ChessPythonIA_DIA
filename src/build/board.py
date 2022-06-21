8055487a9e8ef8d57efe33846ceca2529f306dc6
FanZy0127 <fanzy28@hotmail.fr>
2022-06-21 12:26:08 +0200

    Implementing straight wanted moves for pawns.

--- src/build/board.py
+++ src/build/board.py
@@ -24,7 +24,7 @@ class Board:
         row_pieces, row_pawns = (7, 6) if color == 'white' else (0, 1)
 
         # Initialization of the Pawns
-        for column in range (COLUMNS):
+        for column in range(COLUMNS):
             self.squares[row_pawns][column] = Square(row_pawns, column, Pawn(color))
 
         # Initialization of the Rooks
@@ -45,7 +45,42 @@ class Board:
         # Initialization of the Kings
         self.squares[row_pieces][4] = Square(row_pieces, 4, King(color))
 
+    @staticmethod
+    def move(piece, base_row, base_column, allowed_move_row, allowed_move_column):
+        base_square = Square(base_row, base_column)
+        final_square = Square(allowed_move_row, allowed_move_column)
+
+        # print(piece.name, f'Base Square {(base_row, base_column)}',
+        #       f'Destination {(allowed_move_row, allowed_move_column)}',
+        #       f'Direction {piece.direction if isinstance(piece, Pawn) else None}')
+
+        move = Move(base_square, final_square)
+        piece.add_move(move)
+
     def calculate_allowed_moves(self, piece, row, column):
+
+        def pawn_moves():
+            allowed_steps = 1 if piece.has_moved else 2
+
+            # Regular moves (vertical)
+            start_of_loop = row + piece.direction
+            # exclusive so excluded from the loop
+            end_of_loop = row + piece.direction * (1 + allowed_steps)  # Brackets needed for the white pawns destination
+
+            # The loop then goes from [1;3[ for 1st move or [1;2[ after 1st move, mathematically speaking
+            for allowed_move_row in range(start_of_loop, end_of_loop, piece.direction):
+                if Square.is_in_range(allowed_move_row):
+                    if self.squares[allowed_move_row][column].is_empty():
+                        self.move(piece, row, column, allowed_move_row, column)
+                    else:
+                        break  # If there's already a piece in front of our pawn, it can't move straight
+                else:
+                    break  # Meaning the wanted square is not in range
+
+            # Attacking moves (diagonal)
+
+        def rook_moves():
+            pass
 
         def knight_moves():
             # A Knight has a maximum of 8 allowed moves
@@ -57,28 +92,28 @@ class Board:
 
             for allowed_move_row, allowed_move_column in allowed_moves:
                 if Square.is_in_range(allowed_move_row, allowed_move_column):  # check if out of the board
-                    if self.squares[allowed_move_row][allowed_move_column].is_empty_or_has_an_opponent_piece(piece.color):
+                    if self.squares[allowed_move_row][allowed_move_column].is_empty_or_has_an_opponent_piece(
+                            piece.color):
+                        self.move(piece, row, column, allowed_move_row, allowed_move_column)
 
-                        base_square = Square(row, column)
-                        final_square = Square(allowed_move_row, allowed_move_column)  # piece = piece
+        def bishop_moves():
+            pass
 
-                        move = Move(base_square, final_square)
-                        piece.add_move(move)
+        def queen_moves():
+            pass
+
+        def king_moves():
+            pass
 
         if isinstance(piece, Pawn):
-            pass
-
+            pawn_moves()
         elif isinstance(piece, Rook):
-            pass
-
+            rook_moves()
         elif isinstance(piece, Knight):
             knight_moves()
-
         elif isinstance(piece, Bishop):
-            pass
-
+            bishop_moves()
         elif isinstance(piece, Queen):
-            pass
-
+            queen_moves()
         elif isinstance(piece, King):
-            pass
+            king_moves()
